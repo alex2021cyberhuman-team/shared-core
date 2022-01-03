4178b451632c4a765f9b52954d2010846d4afcf9
Alex2021cyberhuman <alex2021cyberhuman@gmail.com>
2022-01-03 19:10:24 +0500

    feat: Add new event models

---
+++ Conduit.Shared/Events/Models/Articles/CreateArticle/CreateArticleEventModel.cs
@@ -0,0 +1,24 @@
+﻿namespace Conduit.Shared.Events.Models.Articles.CreateArticle;
+
+public class CreateArticleEventModel
+{
+    public string Slug { get; set; } = string.Empty;
+
+    public string Title { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public string Body { get; set; } = string.Empty;
+
+    public ISet<string> TagList { get; set; } = new HashSet<string>();
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+
+    public string AuthorUsername { get; set; } = string.Empty;
+
+    public string? AuthorBiography { get; set; }
+
+    public string? AuthorImage { get; set; }
+}
---
+++ Conduit.Shared/Events/Models/Articles/DeleteArticle/DeleteArticleEventModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Conduit.Shared.Events.Models.Articles.DeleteArticle;
+
+public class DeleteArticleEventModel
+{
+    public string Slug { get; set; } = string.Empty;
+
+    public string AuthorUsername { get; set; } = string.Empty;
+}
