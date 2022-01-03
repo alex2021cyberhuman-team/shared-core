8f727be13602664c07a41b4eaffdb896c480af68
Alex2021cyberhuman <alex2021cyberhuman@gmail.com>
2022-01-03 18:24:03 +0500

    feat: Add validation attribute

---
+++ Conduit.Shared/Validation/NestedValidationAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Conduit.Shared.Validation;
+
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter)]
+public class NestedValidationAttribute : ValidationAttribute
+{
+    protected override ValidationResult? IsValid(
+        object? value,
+        ValidationContext validationContext)
+    {
+        if (value is null)
+        {
+            return null;
+        }
+
+        var newContext = new ValidationContext(value)
+        {
+            DisplayName = validationContext.DisplayName,
+            MemberName = validationContext.MemberName
+        };
+        
+        var results = new List<ValidationResult>();
+        _ = Validator.TryValidateObject(value, newContext, results);
+        return results.FirstOrDefault();
+    }
+}
