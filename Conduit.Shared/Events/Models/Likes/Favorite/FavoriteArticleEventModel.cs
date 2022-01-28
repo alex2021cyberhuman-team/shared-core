aec8da0c5d6e05d82a7294245fbc06a6617eda67
Alex2021cyberhuman <alex2021cyberhuman@gmail.com>
2022-01-28 17:55:19 +0500

    fix: Reformat update and so on

--- Conduit.Shared/Events/Models/Likes/Favorite/FavoriteArticleEventModel.cs
+++ Conduit.Shared/Events/Models/Likes/Favorite/FavoriteArticleEventModel.cs
@@ -1,7 +1,6 @@
-using System.Runtime.Serialization;
 using System.Text.Json.Serialization;
 
-namespace Conduit.Shared.Events.Models.Favorites;
+namespace Conduit.Shared.Events.Models.Likes.Favorite;
 
 public class FavoriteArticleEventModel
 {
