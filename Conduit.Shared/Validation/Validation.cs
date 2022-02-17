b076b2f836c84124502859d16a69d1fab4a26d30
Alex2021cyberhuman <alex2021cyberhuman@gmail.com>
2022-02-17 22:26:43 +0500

    feat: Add validation and comment event models

---
+++ Conduit.Shared/Validation/Validation.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Conduit.Shared.Validation;
+
+public class Validation
+{
+    public Validation(
+        List<ValidationResult>? results = null)
+    {
+        Results = results ?? new();
+    }
+
+    public List<ValidationResult> Results { get; set; }
+
+    public static implicit operator bool(
+        Validation validation)
+    {
+        return validation.Results.Any() == false;
+    }
+}
